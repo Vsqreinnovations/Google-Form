02166d2c666e8ee526a90aa0e06eac2842a488b0
Vsqre Innovations <vsqreinnovations@gmail.com>
2025-06-15 04:29:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+ const form = document.getElementById("courseForm");
+    const popup = document.getElementById("popupMessage");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const formData = new FormData(form);
+
+      fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+        .then(response => {
+          if (response.ok) {
+            popup.textContent = "✅ Registration Successful! Thank you.";
+            popup.classList.add("show");
+            form.reset();
+          } else {
+            popup.textContent = "❌ Something went wrong. Try again.";
+            popup.classList.add("show");
+          }
+        })
+        .catch(error => {
+          popup.textContent = "⚠️ Error submitting form.";
+          popup.classList.add("show");
+          console.error("Error:", error);
+        });
+    });
+
+    form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const confirmSend = confirm("Have you sent your payment proof to our WhatsApp (+91 93422 45687)?");
+    if (!confirmSend) return;
+
+    const formData = new FormData(form);
+    fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                popup.textContent = "✅ Registration Successful! Thank you.";
+                popup.classList.add("show");
+                form.reset();
+            } else {
+                popup.textContent = "❌ Something went wrong. Try again.";
+                popup.classList.add("show");
+            }
+        })
+        .catch(error => {
+            popup.textContent = "⚠️ Error submitting form.";
+            popup.classList.add("show");
+            console.error("Error:", error);
+        });
+});
